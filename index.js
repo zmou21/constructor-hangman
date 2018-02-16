14a5c20319bc6d1359b29db54c2d52919e4387f9
Zack <zmoumen13@gmail.com>
2018-02-16 09:24:20 -0700

    updated JS files

---
+++ index.js
@@ -0,0 +1,23 @@
+// index.js: The file containing the logic for the course of the game, which depends on Word.js and:
+
+
+// Randomly selects a word and uses the Word constructor to store it
+// Prompts the user for each guess and keeps track of the user's remaining guesses
+
+// function guess() {
+// 	inquirer.prompt([
+// 	{
+// 	type: "input",
+// 	name: "letter",
+// 	message: "Guess a letter:"	
+// 	}	
+// 	]).then(function(result) {
+// 		if(result.letter){
+// 			var letter = new Letter(result.letter.toLowerCase());
+// 			console.log(result.letter);
+// 		}
+// 		else{
+// 			console.log("guess again");
+// 		}
+// 	});
+// };
---
+++ letter.js
@@ -0,0 +1,44 @@
+// Letter.js: Contains a constructor, Letter. This constructor should be able to either display an underlying character 
+// or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the 
+// constructor should define:
+
+	// A string value to store the underlying character for the letter
+	// A boolean value that stores whether that letter has been guessed yet
+	// A function that returns the underlying character if the letter has been guessed, 
+	// 		or a placeholder (like an underscore) if the letter has not been guessed
+	// A function that takes a character as an argument and checks it against the underlying character, 
+	// 		updating the stored boolean value to true if it was guessed correctly
+
+var inquirer = require("inquirer");
+
+
+var Letter = function(letter) {
+	this.letter = letter.toLowerCase();
+	this.value = false;
+	this.display = function() {
+		var placeholder = "_";
+		if(this.letter === letter) {
+			//console.log("continue");
+			return letter;
+		}
+		else if (this.letter !== letter){
+			return placeholder;
+		}
+	};
+	this.character = function() {
+		if (this.letter === letter) {
+			value = true;
+		}
+		else {
+			value = false;
+		}
+	};
+};
+
+var letter = new Letter("B");
+console.log(letter.letter);
+//console.log(letter.value);
+console.log(letter.display());
+console.log(letter.character());
+
+module.exports = Letter;
