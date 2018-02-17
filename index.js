2fd5e66447c3f2797e450f25b8895196329713fe
Zack <zmoumen13@gmail.com>
2018-02-17 12:57:56 -0700

    updated index file with inquirer

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 // Randomly selects a word and uses the Word constructor to store it
 // Prompts the user for each guess and keeps track of the user's remaining guesses
 
+var inquirer = require("inquirer");
+
 // function guess() {
 // 	inquirer.prompt([
 // 	{
